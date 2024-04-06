f2cb198663563ba9de2911d29181b3f1b29c464b
belinchio <belinchio@yandex.ru>
2024-04-06 20:33:52 +0300

    release

--- MyVector.h
+++ MyVector.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <algorithm>
+#include <cstddef>
 #include <iostream>
 #include <iterator>
 #include <initializer_list>
@@ -9,6 +11,7 @@
 template <typename T>
 class Vector {
 private:
+
     T* m_buffer = nullptr;
     size_t m_size = 0u;
     size_t m_capacity = 0;
@@ -27,6 +30,8 @@ private:
     }
 
 public:
+
+    //---------------Конструкторы-/-деструкторы---------------
 
     // Конструктор по умолчанию
     Vector() = default;
@@ -58,23 +63,17 @@ public:
 
     // Деструктор
     ~Vector() {
+        clear();
         delete[] m_buffer;
     }
 
-    /* Итераторы
-     *      begin/cbegin возвращает итератор на начало
-     *      end/cend возвращает итератор на конеч
-     *      rbegin/crbegin возвращает обратный итератор на начало
-     *      rend/crend вщзвращает обратный итератор на конец
+    //---------------Итераторы---------------
+    /* 
+     * begin/cbegin возвращает итератор на начало
+     * end/cend возвращает итератор на конеч
+     * rbegin/crbegin возвращает обратный итератор на начало
+     * rend/crend вщзвращает обратный итератор на конец
      */
-
-    const T& operator [](size_t index) const noexcept {
-        return const_cast<Vector&>(*this)[index];
-    }
-
-    T& operator [](size_t index) noexcept {
-        return m_buffer[index];
-    }
 
     T* begin() const {
         return m_buffer;
@@ -108,6 +107,51 @@ public:
         return std::reverse_iterator<const T*>(m_buffer);
     }
 
+    //---------------Доступ-к-элементам---------------
+
+    T& at(const size_t index) {
+        if (index >= m_size) {
+            throw std::out_of_range("Index out of range");
+        }
+        return m_buffer[index];
+    }
+
+    const T& operator[] (size_t index) const noexcept {
+        return const_cast<Vector&>(*this)[index];
+    }
+
+    T& operator[] (size_t index) noexcept {
+        return m_buffer[index];
+    }
+
+    Vector<T>& operator= (const Vector<T>& other) {
+        Vector<T> copy(other);
+        swap(copy);
+    }
+
+    constexpr const T& front() const {
+        return *begin();
+    }
+
+    constexpr const T& back() const {
+        return *std::prev(end());
+    }
+
+    constexpr T* data() noexcept {
+        return m_buffer;
+    }
+
+    constexpr const T* data() const noexcept {
+        return m_buffer;
+    }
+
+    //---------------Ёмкость-контейнера---------------
+
+    // Метод для проверки пуст ли вектор
+    [[nodiscard]] bool empty() const noexcept {
+        return m_size == 0;
+    }
+
     // Метод для получения текущего размера вектора
     [[nodiscard]] size_t size() const noexcept {
         return m_size;
@@ -118,30 +162,39 @@ public:
         return std::numeric_limits<size_t>::max();
     }
 
+    // Метод резервирования памяти
+    void reserve(size_t new_capacity) {
+        if (new_capacity <= m_capacity) {return;}
+        
+        T* new_buffer = new T[new_capacity];
+        if (m_buffer != nullptr) {
+            std::copy(m_buffer, m_buffer + m_size, new_buffer);
+            delete [] m_buffer;
+        }
+        m_buffer = new_buffer;
+        m_capacity = new_capacity;
+    }
+
     // Метод для получения текущей вместимости вектора
     [[nodiscard]] size_t capacity() const noexcept {
         return m_capacity;
     }
 
-    // Метод для проверки "пуст ли" вектор
-    [[nodiscard]] bool empty() const noexcept {
-        return m_size == 0;
+    void shrinnk_to_fit() noexcept {
+        if (m_buffer == nullptr || m_capacity == m_size) {return;}
+
+        T* new_buffer = new T[m_size];
+        std::copy(m_buffer, m_buffer + m_size, new_buffer);
+        delete [] m_buffer;
+        m_buffer = new_buffer;
+        m_capacity = m_size;
     }
 
-    // Метод для добавления элемента в конец вектора
-    void push_back(const T& value) {
-        if (m_size == m_capacity) {
-            realloc();
-        }
-        m_buffer[m_size++] = value;
-    }
+    //---------------Модификаторы---------------
 
-    // Метод для удаления последнего элемента
-    void pop_back() {
-        if (empty()) {
-            throw std::out_of_range("Vector is empty");
-        }
-        m_buffer[m_size--];
+    // Метод очистки вектора
+    void clear() {
+        m_size = 0;
     }
 
     // Метод для вставки элемента по указанному индексу
@@ -163,6 +216,15 @@ public:
         }
     }
 
+    //Метод для ставки элемента с заменой по указаному индексу
+    template<typename Args>
+    void emplace(const int index, Args&& args) const {
+        if (index < 0 && index >= m_size) {
+            throw std::out_of_range("Index out of range");
+        }
+        m_buffer[index] = T(std::forward<Args>(args));
+    }
+
     // Метод для удаления элемента по указанному индуксу
     void erase(int index) {
         if (index < 0 || index >= m_size) {
@@ -174,23 +236,22 @@ public:
         m_size--;
     }
 
-    // Метод для доступа к элементу по указанному индексу
-    T& at(const size_t index) {
-        if (index >= m_size) {
-            throw std::out_of_range("Index out of range");
+    // Метод для добавления элемента в конец вектора
+    void push_back(const T& value) {
+        if (m_size == m_capacity) {
+            realloc();
         }
-        return m_buffer[index];
+        m_buffer[m_size++] = value;
     }
 
-    //Метод для ставки элемента с заменой по указаному индексу
-    template<typename Args>
-    void emplace(const int index, Args&& args) const {
-        if (index < 0 && index >= m_size) {
-            throw std::out_of_range("Index out of range");
+    // Метод для удаления последнего элемента
+    void pop_back() {
+        if (empty()) {
+            throw std::out_of_range("Vector is empty");
         }
-        m_buffer[index] = T(std::forward<Args>(args));
+        m_buffer[m_size--];
     }
-
+    
     //Метод для изменения размера вектора
     void resize(size_t new_size) {
         if (new_size < 0) {
@@ -217,30 +278,4 @@ public:
         std::swap(m_size, other.m_size);
         std::swap(m_capacity, other.m_capacity);
     }
-
-    constexpr const T& front() const {
-        return *begin();
-    }
-
-    constexpr const T& back() const {
-        return *std::prev(end());
-    }
-
-    constexpr T* data() noexcept {
-        return m_buffer;
-    }
-
-    constexpr const T* data() const noexcept {
-        return m_buffer;
-    }
-
-//    constexpr T& at(size_t index) {
-//        try {
-//            if (index >= 0 && index < m_size) {
-//                return m_buffer[index];
-//            }
-//        } catch (std::exception& e) {
-//            std::cerr << e.what() << std::endl;
-//        }
-//    }
 };
--- main.cpp
+++ main.cpp
@@ -1,101 +1,41 @@
 #include "MyVector.h"
 
-int main()
-{
+#include <iostream>
+
+// Функция вывода содержимого вектора в консоль
+template<typename T>
+void PrintVector(T temp) {
+    for (const auto& elem : temp) {
+        std::cout << elem << " ";
+    }
+    std::cout << "\n" << std::endl;
+    std::cout << "Размер контейнера size  = " << temp.size() << " byte" << std::endl;
+    std::cout << "Ёмкость контейнера capacity = " << temp.capacity() << " byte" << std::endl;
+    std::cout << std::endl;
+}
+
+int main(int argc, char const *argv[])
+{   
+    // Создаем пустой вектор vec типа int 
     Vector<int> vec;
-    vec.push_back(10);
-    vec.push_back(20);
-    std::cout << vec.empty() << std::endl;
-
-    for (const auto& el : vec) {
-        std::cout << el << " ";
+    // заполняем вектор значениями от 0 до 9
+    for (auto i = 0; i < 10; ++i) {
+        vec.push_back(i);
     }
-    std::cout.put('\n');
+    PrintVector(vec);
 
-    Vector<int> num {1, 2, 3, 4, 5 ,6, 7, 8, 9};
-    for (const auto& el : num) {
-        std::cout << el << " ";
-    }
-    std::cout<< std::endl;
-    std::cout << num.size() << std::endl;
+    // Удаляем последний элемент вектрора и вставляем значение '43' по индексу 5
+    vec.pop_back();
+    vec.insert(5, 43);
+    PrintVector(vec);
 
-    //num.pop_back();
+    // Создаем инициализированный вектор str типа std::string
+    Vector<std::string> str {"The", "World", "Is", "Mine", "!"};
+    PrintVector(str);
 
+    // Меняем значение в позиции с индексом 4
+    str.emplace(4, "?");
+    PrintVector(str);
 
-    for (const auto& el : num) {
-        std::cout << el << " ";
-    }
-    std::cout<< std::endl;
-
-    num.insert(2, 56);
-
-    for (const auto& el : num) {
-        std::cout << el << " ";
-    }
-    std::cout<< std::endl;
-    std::cout << num.size() << std::endl;
-
-
-    num.insert(5, 65);
-    for (const auto& el : num) {
-        std::cout << el << " ";
-    }
-    std::cout<< std::endl;
-    std::cout << num.size()<< std::endl;
-    num.insert(12, 45);
-    std::cout << num.size()<< std::endl;
-    num.insert(12, 45);
-    std::cout << num.size()<< std::endl;
-
-    num.insert(12, 45);
-    std::cout << num.size()<< std::endl;
-    num.insert(-4, 78);
-    for (const auto& el : num) {
-        std::cout << el << " ";
-    }
-    std::cout<< std::endl;
-//
-    std::cout << num.size() << " " << num.capacity() << "\n";
-//    num.erase(2);
-//    for (const auto& el : num) {
-//        std::cout << el << " ";
-//    }
-//    std::cout<< std::endl;
-
-//    num.emplace(4, 15);
-//    for (const auto& el : num) {
-//        std::cout << el << " ";
-//    }
-//    std::cout<< std::endl;
-
-//    std::cout << num.max_size() << std::endl;
-
-    //Vector<int> g(10);
-
-    Vector<char> str {'o', 'f', 'c'};
-    //Vector<char> str;
-    std::cout << "size - " << str.size() << " capacity - " << str.capacity() << std::endl;
-    std::cout << "первый элемент - " << str.front() << std::endl;
-    std::cout << "последний элемент - " << str.back() << std::endl;
-
-    Vector<size_t> d {1, 2, 3, 4, 5};
-    std::cout << "первый элемент - " << d.front() << std::endl;
-    std::cout << "последний элемент - " << d.back() << std::endl;
-    std::cout << str.at(0) << std::endl;
-    std::cout << std::endl;
-    std::cout << "/////////////////////" << std::endl;
-    Vector<int> v {1, 2, 3, 4, 5, 6, 7};
-    Vector<double> v3;
-    Vector<std::string> v1;
-
-    std::cout << &v[0] << "\t" << &v1[0] << "\t" << &v3[0] << std::endl; //
-    std::cout << v.data() << "\t" << v1.data() << "\t" << v3.data() << std::endl;
-    std::cout << "-----------------------------" << std::endl;
-
-    Vector<std::string> s {"MY", "LOVE", "IS"};
-    for (auto i : s) {
-        std::cout << i << " ";
-    }
-    std::cout << std::endl;
     return 0;
 }
